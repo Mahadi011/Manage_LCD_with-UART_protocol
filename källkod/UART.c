956af83282c9ea5166863378c5a09ceac6a46b16
Mahadi Hasan <113011503+Mahadi011@users.noreply.github.com>
2023-05-02 11:04:58 +0200

    updated

--- källkod/UART.c
+++ källkod/UART.c
@@ -13,23 +13,39 @@ void UART_Config(void)
     3. Configure USART 	
     */
 
+
+   /* The USART2 module can be configured to use different clock sources, 
+   such as the system clock or an external clock. Enabling the USART2 clock 
+   allows us to set the desired clock source and configure the module 
+   accordingly.
+   
+   GPIOA pins can be configured as inputs or outputs, allowing us to read 
+   signals from external sensors or control external devices such as LEDs 
+   or motors. Enabling the GPIOA clock allows us to configure the pins as 
+   inputs or outputs and use them for our application.
+    */
    // Enable USART2 clock and GPIOA clock
    RCC-> APB1ENR |= 0x20000; // Enable USART2 clock
    RCC-> AHB1ENR |=0x00001;  // Enable GPIOA clock
+
+   /* The USART2 module is a hardware peripheral that needs to be enabled 
+   before it can be used. Enabling the USART2 clock allows the MCU to 
+   access the USART2 module and use its features.
+    */
 
    //Configure the USART PINs for ALternate Functions
    GPIOA-> MODER |= 0x000A0; //Bits7:6 = 1:0 and bits 5:4 = 1:0 to set pin PA3 and PA2 in alternet mode
    GPIOA->OTYPER|= 0x0000C  // bit 3=1 and bit 2=1 output open drain 
    GPIOA->OSPEEDR |= 0x000F0 // Bits 7:6 =1:1 and bits 5:4 = 1:1 to set pin PA3 and PA2 in high speed 
    GPIOA ->PUPDR |= 0x00050 // Bits 7:6 =0:1 and bits 5:4 = 0:1 to set pin PA3 and PA2 in pull up
-   GPIOA -> AFR[0] |= 0x07700  // Bits 15:14:13:12 =0:1:1:1 and bits 11:10:9:8 = 0:1:1:1 to set pin PA3 and PA2 in alternate function
+   GPIOA -> AFR[0] |= 0x07700  // Bits 15:14:13:12 =0:1:1:1 and bits 11:10:9:8 = 0:1:1:1 to set pin PA3 and PA2 in alternate function 
 
    //Reset the USART
     USART2->BRR |= 0x0683; // setting standerd baud-rate which is 9600 bps.
     USART2->CR1 |= 0x0000C; // Activate transmit and recever by putting 1 in bit 2 and 3
     USART2->CR2 |= 0x00000; // deactivate control resister 2
     USART2->CR3 |= 0X00000; // deactivate control resister 3
-    USART2->CR1 |= 0x02000; // enable usart by put 1 in bit bo 13 
+    USART2->CR1 |= 0x02000; // enable usart by put 1 in bit  13 
 }
 
 void UART_Write(uint8_t data)
